57ba6640d7468061b0f14d7c23340faefb624d98
amparisa <6306021622096@fitm.kmutnb.ac.th>
2022-03-23 15:18:52 +0700

    Start Use Git

---
+++ lab1/index.js
@@ -0,0 +1,1 @@
+console.log('Hello Javascript is very very good');
---
+++ lab2/nummath.js
@@ -0,0 +1,16 @@
+let num1 = 100;
+//Basic Math
+console.log("******Basic Math*****");
+console.log(num1 + 25);
+console.log(num1 - 100);
+console.log(num1 * 100);
+console.log(num1 / 1500);
+console.log("********AdditionaMathematical Operatons***");
+//Additional Mathematical Operations
+console.log(num1 % 1500); // Remainder
+console.log(++num1); //Increment
+console.log(--num1); //Decrement
+console.log("*****Usingthe Math Object********");
+//Using the Math Object
+console.log(Math.PI);//Pi
+console.log(Math.sqrt(num1) ) ; //Square root
